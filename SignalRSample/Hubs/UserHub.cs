5892a3a15e5a4ad02dd7ba5428899c9e52e03c24
Bhrugen Patel <bhrugen1990@gmail.com>
2022-05-02 22:57:38 -0500

    Section 2 - Create Hub Method

--- SignalRSample/Hubs/UserHub.cs
+++ SignalRSample/Hubs/UserHub.cs
@@ -4,5 +4,14 @@ namespace SignalRSample.Hubs
 {
     public class UserHub : Hub
     {
+        public static int TotalViews { get; set; } = 0;
+
+        public async Task NewWindowLoaded()
+        {
+            TotalViews++;
+            //send update to all clients that total views have been updated
+            await Clients.All.SendAsync("updateTotalViews", TotalViews);
+        }
+
     }
 }
