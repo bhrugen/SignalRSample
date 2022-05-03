362c5418ec3c2a909d8e2ffbfce8fcb8b0edfe86
Bhrugen Patel <bhrugen1990@gmail.com>
2022-05-02 23:33:24 -0500

    Section 2 - Configure SignalR Client Js

---
+++ SignalRSample/wwwroot/js/usersCount.js
@@ -0,0 +1,25 @@
+﻿
+//create connection
+var connectionUserCount = new signalR.HubConnectionBuilder().withUrl("/hubs/userCount").build();
+
+//connect to methods that hub invokes aka receive notfications from hub
+connectionUserCount.on("updateTotalViews", (value) => {
+    var newCountSpan = document.getElementById("totalViewsCounter");
+    newCountSpan.innerText = value.toString();
+});
+
+//invoke hub methods aka send notification to hub
+function newWindowLoadedOnClient() {
+    connectionUserCount.send("NewWindowLoaded");
+}
+
+//start connection
+function fulfilled() {
+    //do something on start
+    console.log("Connection to User Hub Successful");
+}
+function rejected() {
+    //rejected logs
+}
+
+connectionUserCount.start().then(fulfilled, rejected);
