eb357f4cb47b54de1968e44ccafb868486a66d60
Bhrugen Patel <bhrugen1990@gmail.com>
2022-05-05 17:46:49 -0500

    Section 5 - Assignment 3 Solution - Notification In Action

--- SignalRSample/wwwroot/js/notification.js
+++ SignalRSample/wwwroot/js/notification.js
@@ -4,6 +4,17 @@ var connectionNotification = new signalR.HubConnectionBuilder()
 
 document.getElementById("sendButton").disabled = true;
 
+connectionNotification.on("LoadNotification", function (message, counter) {
+    document.getElementById("messageList").innerHTML = "";
+    var notificationCounter = document.getElementById("notificationCounter");
+    notificationCounter.innerHTML = "<span>(" + counter + ")</span>";
+    for (let i = message.length - 1; i >= 0; i--) {
+        var li = document.createElement("li");
+        li.textContent = "Notification - " + message[i];
+        document.getElementById("messageList").appendChild(li);
+    }
+});
+
 document.getElementById("sendButton").addEventListener("click", function (event) {
     var message = document.getElementById("notificationInput").value;
     connectionNotification.send("SendMessage", message).then(function () {
