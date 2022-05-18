6bed2fc7798acda8a721ead9b4ab6071d857ca08
Bhrugen Patel <bhrugen1990@gmail.com>
2022-05-18 14:53:17 -0500

    Section 9 - API Endpoints for Chat Application

---
+++ SignalRSample/Controllers/ChatRoomsController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SignalRSample.Data;
+using SignalRSample.Models;
+
+namespace SignalRSample.Controllers
+{
+    [Route("/[controller]")]
+    [ApiController]
+    public class ChatRoomsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ChatRoomsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ChatRooms
+        [HttpGet]
+        [Route("/[controller]/GetChatRoom")]
+        public async Task<ActionResult<IEnumerable<ChatRoom>>> GetChatRoom()
+        {
+          if (_context.ChatRoom == null)
+          {
+              return NotFound();
+          }
+            return await _context.ChatRoom.ToListAsync();
+        }
+
+        [HttpGet]
+        [Route("/[controller]/GetChatUser")]
+        public async Task<ActionResult<Object>> GetChatUser()
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var users = await _context.Users.ToListAsync();
+
+            if (users == null)
+            {
+                return NotFound();
+            }
+
+            return users.Where(u => u.Id != userId).Select(u => new { u.Id, u.UserName }).ToList();
+        }
+
+
+
+
+        // POST: api/ChatRooms
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        [Route("/[controller]/PostChatRoom")]
+        public async Task<ActionResult<ChatRoom>> PostChatRoom(ChatRoom chatRoom)
+        {
+          if (_context.ChatRoom == null)
+          {
+              return Problem("Entity set 'ApplicationDbContext.ChatRoom'  is null.");
+          }
+            _context.ChatRoom.Add(chatRoom);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetChatRoom", new { id = chatRoom.Id }, chatRoom);
+        }
+
+        // DELETE: api/ChatRooms/5
+        [HttpDelete("{id}")]
+        [Route("/[controller]/DeleteChatRoom/{id}")]
+        public async Task<IActionResult> DeleteChatRoom(int id)
+        {
+            if (_context.ChatRoom == null)
+            {
+                return NotFound();
+            }
+            var chatRoom = await _context.ChatRoom.FindAsync(id);
+            if (chatRoom == null)
+            {
+                return NotFound();
+            }
+
+            _context.ChatRoom.Remove(chatRoom);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        
+    }
+}
