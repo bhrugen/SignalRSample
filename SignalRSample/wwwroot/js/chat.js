ec30ddebb15940f43e125eb90668ad1b6ed71199
Bhrugen Patel <bhrugen1990@gmail.com>
2022-05-18 16:35:35 -0500

    Section 9 - Notify User is online

--- SignalRSample/wwwroot/js/chat.js
+++ SignalRSample/wwwroot/js/chat.js
@@ -1,1 +1,24 @@
 ﻿
+var connection = new signalR.HubConnectionBuilder()
+    .withUrl("/hubs/chat")
+    .withAutomaticReconnect([0, 1000, 5000, null])
+    .build();
+
+
+connection.on("ReceiveUserConnected", function (userId, userName) {
+    addMessage(`${userName} is online`);
+});
+
+
+
+function addMessage(msg) {
+    if (msg == null && msg == '') {
+        return;
+    }
+    let ui = document.getElementById('messagesList');
+    let li = document.createElement("li");
+    li.innerHTML = msg;
+    ui.appendChild(li);
+}
+
+connection.start();
